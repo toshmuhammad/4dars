869e037b1f4ef21596ccd320bc91565afd9efe93
toshmuhammad <toshmuhammad@gmail.com>
2025-02-01 21:41:20 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let wrapDiv = document.querySelector(".wrap")
+
+let h1 = document.createElement("h1")
+h1.textContent = "Assalomu alaykum";
+h1.style.color = "blue";
+h1.style.fontSize = "24px";
+
+let p = document.createElement("p")
+p.textContent = "Valaykum assalom";
+p.style.color = "red";
+p.style.color = "24px";
+
+wrapDiv.appendChild(h1);
+wrapDiv.appendChild(p)
+
+
+
+
+
+
+// let container = document.querySelector("#container")
+
+// let fristParagraph = container.firstElementChild;
+// let lastParagraph = container.lastElementChild;
+
+// fristParagraph.textContent = "Uzbek";
+// lastParagraph.textContent = "Rus";
+
+
+
+
+
+// let listItems = document.querySelectorAll("ul li");
+// listItems.forEach(item => {
+//     item.style.backgroundColor = "yellow";
+// });
